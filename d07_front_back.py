efd8e95f4141d8424872d28f33ea277f11e026d2
Leticia Campos Valente <77363260+leticiacamposv@users.noreply.github.com>
2022-02-05 15:57:57 -0300

    Add files via upload

---
+++ d07_front_back.py
@@ -0,0 +1,53 @@
+"""
+07. front_back
+
+Considere dividir uma string em duas metades.
+Caso o comprimento seja par, a metade da frente e de trás tem o mesmo tamanho.
+Caso o comprimento seja impar, o caracter extra fica na metade da frente.
+
+Exemplo: 'abcde', a metade da frente é 'abc' e a de trás é 'de'.
+
+Finalmente, dadas duas strings a e b, retorne uma string na forma:
+a-frente + b-frente + a-trás + b-trás
+"""
+
+
+def front_back(a, b):
+    strings = [a, b]
+    newlist = []
+    for param in strings:
+        if len(param) % 2 == 0:
+            newlist.append(param[:int((len(param) / 2))])
+            newlist.append(param[int((len(param) / 2)):])
+        else:
+            newlist.append(param[:int((len(param) / 2) + 1)])
+            newlist.append(param[int((len(param) / 2) + 1):])
+
+    newstr = newlist[0] + newlist[2] + newlist[1] + newlist[3]
+    return newstr
+
+
+# --- Daqui para baixo são apenas códigos auxiliáries de teste. ---
+
+def test(f, in_, expected):
+    """
+    Executa a função f com o parâmetro in_ e compara o resultado com expected.
+    :return: Exibe uma mensagem indicando se a função f está correta ou não.
+    """
+    out = f(*in_)
+
+    if out == expected:
+        sign = '✅'
+        info = ''
+    else:
+        sign = '❌'
+        info = f'e o correto é {expected!r}'
+
+    print(f'{sign} {f.__name__}{in_!r} retornou {out!r} {info}')
+
+
+if __name__ == '__main__':
+    # Testes que verificam o resultado do seu código em alguns cenários.
+    test(front_back, ('abcd', 'xy'), 'abxcdy')
+    test(front_back, ('abcde', 'xyz'), 'abcxydez')
+    test(front_back, ('Kitten', 'Donut'), 'KitDontenut')
